69d826c204a31d23f1d2cda66c38db8d9fc5ae73
Ronaldo <ronaldoandrade966@gmail.com>
2022-10-15 21:07:22 -0300

    database url from .env

--- src/db.js
+++ src/db.js
@@ -1,13 +1,12 @@
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const { Pool } = pg;
 
 const connection = new Pool ({
-    user: "postgres",
-    password: "orkutorkut",
-    host: "localhost",
-    port: 5432,
-    database: "shortly"
+    connectionString: process.env.DATABASE_URL
 });
 
 export default connection;
